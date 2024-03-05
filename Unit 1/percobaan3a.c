bf153067bae48f10b1add2bd785b6dfb89d90292
Fakhtur17 <162319031+Fakhtur17@users.noreply.github.com>
2024-03-05 19:09:48 +0700

    Create percobaan3a.c

---
+++ Unit 1/percobaan3a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  printf("Baris pertama");
+  printf("Baris kedua \n");
+  printf("Baris ketiga");
+  return 0;
+}
+
+//fungsi \n sebagai tanda end line atau untuk turun baris
+//outputnya akan menjadi 
+//Baris pertamaBaris kedua
+//Baris ketiga
